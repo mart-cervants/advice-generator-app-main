d86af0a77bd91172225a3c0812a3ae82593cd23f
MACB <mart.cervants@gmail.com>
2022-03-30 11:13:00 -0600

    Upload files

---
+++ app.js
@@ -0,0 +1,19 @@
+const resDiv = document.querySelector('.advice');
+const resAdvId = document.querySelector('.adv-id');
+
+window.onload = () => {
+  getAdvice();
+}; 
+ 
+function getAdvice() {
+  fetch('https://api.adviceslip.com/advice').then(response => {
+    return response.json();
+  }).then(adviceData => {
+    const Adviceobj = adviceData.slip;
+    resAdvId.innerHTML = resAdvId.innerHTML + Adviceobj.id;
+    resDiv.innerHTML = "\“"+Adviceobj.advice+"\”";
+  }).catch(error => {
+    console.log(error);
+  });
+ 
+}
